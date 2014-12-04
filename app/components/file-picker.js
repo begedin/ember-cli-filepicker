85a59afd9c7f9f6ea9a32ae2c4ceb282b8ebedfc
Mike Grassotti <mgrassotti@gmail.com>
2014-12-04 12:00:06 -0500

    Working test that verifies results after filepicker widget is clicked, needs refactoring

--- app/components/file-picker.js
+++ app/components/file-picker.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  handleElementChanged: function(e) {
+    this.set("fpfiles", e.fpfiles);
+  },
   setupFilepicker: function() {
     var element = this.$('input')[0];
+    var self = this;
     this.get('filepicker').then( function(filepicker) {
       element.type="filepicker";
+      if (Ember.testing) {
+        element.setAttribute("data-fp-debug", true);
+      }
+      element.onchange = Ember.run.bind(self, 'handleElementChanged');
       filepicker.constructWidget(element);
     });
   }.on('didInsertElement'),
--- tests/acceptance/demo-test.js
+++ tests/acceptance/demo-test.js
@@ -1,7 +1,42 @@
+/* global waitForPromise */
+/* global waitForElement */
+
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 
 var App;
+
+Ember.Test.registerAsyncHelper('waitForPromise', function(app, name) {
+  return new Ember.Test.promise(function(resolve) {
+    Ember.Test.adapter.asyncStart();
+    var promise = app.__container__.lookup(name);
+    promise.then(function(){
+      Ember.run.schedule('afterRender', null, resolve);
+      Ember.Test.adapter.asyncEnd();
+    });
+  });
+});
+
+Ember.Test.registerAsyncHelper('waitForElement', function(app, selector, timeout) {
+  var elapsed = 0;
+  timeout = timeout || 100;
+  return new Ember.Test.promise(function(resolve, reject) {
+    Ember.Test.adapter.asyncStart();
+    var tick = function() {
+      if (find(selector).length > 0) {
+        Ember.run.schedule('afterRender', null, resolve);
+        Ember.Test.adapter.asyncEnd();
+      } else {
+        if (elapsed > timeout) {
+          throw("waitForElement: timed out waiting for " + selector);
+        } else {
+          elapsed += 10;
+          Ember.run.later(this, tick, 10);
+        }
+      }
+    }();
+  });
+});
 
 module('Acceptance: Demo', {
   setup: function() {
@@ -14,14 +49,18 @@ module('Acceptance: Demo', {
 
 test('file-picker component demo', function() {
   visit('/');
-
   andThen(function() {
     expectComponent('file-picker');
+    expectNoElement('.fpfile');
   });
+});
 
-  // TODO: Figure out how to wait for filepicker api to have loaded
-  // before trying to click on the button...
-  // debugger;
-  // clickComponent('file-picker', 'button');
-
+test('demo page shows file stats after upload', function() {
+  visit('/');
+  waitForPromise("ember-cli-filepicker:api");
+  clickComponent('file-picker', 'button');
+  waitForElement('.fpfile');
+  andThen(function() {
+    expectElement('.fpfile', 1);
+  });
 });
